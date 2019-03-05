771d0dc155937b481578eff629aa56b00af8758e
mathYang1224 <2351112798@qq.com>
2019-03-05 22:01:46 +0800

    pos-master

---
+++ README.md
@@ -0,0 +1,21 @@
+# pos收银系统
+
+>只实现了首页结账
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
+
+For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <leftNav></leftNav>
+    <div id="main">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+import leftNav from '@/components/common/leftNav'
+export default {
+  name: 'App',
+  components:{
+    leftNav
+  }
+}
+</script>
+
+<style scoped>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  height: 100%;
+}
+#main {
+  float: left;
+  width: 95%;
+  background-color: #eff2f7;
+  height: 100%;
+  overflow: auto;
+}
+</style>
